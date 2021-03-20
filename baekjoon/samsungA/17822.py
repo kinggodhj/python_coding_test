4d9d369dbaba3eb9d005be252f881825c26176b1
min_kinggod <heeeee123@ajou.ac.kr>
2021-03-21 00:53:10 +0900

    queue and simulation

--- baekjoon/samsungA/17822.py
+++ baekjoon/samsungA/17822.py
@@ -1,20 +1,23 @@
 from collections import deque
 import copy
 N, M, T=map(int, input().rstrip().split(' '))
-array=[list(map(int, input().rstrip().split(' '))) for _ in range(N)]
-rotation=[list(map(int, input().rstrip().split(' ' ))) for _ in range(T)]
+array=[deque([]) for _ in range(N)]
 queue=deque([])
+total=0
+for i in range(N):
+    l=list(map(int, input().rstrip().split(' ')))
+    array[i].extend(l)
+    total+=sum(l)
+rotation=[list(map(int, input().rstrip().split(' ' ))) for _ in range(T)]
 count=N*M
 
-def rotate(x, d, k, arr):
-    now=arr[x]
+def rotate(x, d, k):
     case=0
     if d==0:
         case=k
     else:
         case=M-k
-    now=now[-case:]+now[:(M-case)]
-    arr[x]=now
+    array[x].rotate(case)
 
 def up(x, y):
     if x<N-1:
@@ -36,84 +39,97 @@ def right(x, y):
         return x, 0
     return x, y+1
 
-def BFS(i, j, check, visited):
+def BFS(visited):
     global count
-    queue.appendleft([i,j])
+    global total
     next_check=False
     while queue:
         remove=False
-        x, y=queue.pop()
+        x, y, v=queue.pop()
+        if v==0:
+            continue
         next_c=up(x, y)
         if next_c is not None:
-            if array[next_c[0]][next_c[1]]==array[x][y] and check[next_c[0]][next_c[1]]!=0:
+            if array[next_c[0]][next_c[1]]==v:
                 visited[next_c[0]][next_c[1]]=1
-                queue.appendleft([next_c[0], next_c[1]])
-                check[next_c[0]][next_c[1]]=0
+                queue.appendleft([next_c[0], next_c[1], v])
+                array[next_c[0]][next_c[1]]=0
+                total-=v
                 count-=1
                 remove=True
         next_c=down(x, y)
         if next_c is not None:
-            visited[next_c[0]][next_c[1]]=1
-            if array[next_c[0]][next_c[1]]==array[x][y] and check[next_c[0]][next_c[1]]!=0:
+            if array[next_c[0]][next_c[1]]==v:
                 visited[next_c[0]][next_c[1]]=1
-                queue.appendleft([next_c[0], next_c[1]])
-                check[next_c[0]][next_c[1]]=0
+                queue.appendleft([next_c[0], next_c[1], v])
+                array[next_c[0]][next_c[1]]=0
+                total-=v
                 count-=1
                 remove=True
         next_c=right(x, y)
         if next_c is not None:
-            if array[next_c[0]][next_c[1]]==array[x][y] and check[next_c[0]][next_c[1]]!=0:
+            if array[next_c[0]][next_c[1]]==v:
                 visited[next_c[0]][next_c[1]]=1
-                queue.appendleft([next_c[0], next_c[1]])
-                check[next_c[0]][next_c[1]]=0
+                queue.appendleft([next_c[0], next_c[1], v])
+                array[next_c[0]][next_c[1]]=0
+                total-=v
                 count-=1
                 remove=True
         next_c=left(x, y)
         if next_c is not None:
-            if array[next_c[0]][next_c[1]]==array[x][y] and check[next_c[0]][next_c[1]]!=0:
+            if array[next_c[0]][next_c[1]]==v:
                 visited[next_c[0]][next_c[1]]=1
-                queue.appendleft([next_c[0], next_c[1]])
-                check[next_c[0]][next_c[1]]=0
+                queue.appendleft([next_c[0], next_c[1], v])
+                array[next_c[0]][next_c[1]]=0
+                total-=v
                 count-=1
                 remove=True
-        if remove==True:
+        if remove==True and array[x][y]!=0:
             next_check=True
-            check[x][y]=0
+            array[x][y]=0
+            total-=v
             count-=1
-    return visited, check, next_check
+    return visited, next_check
 
 def find_mean(arr):
     global count
-    total=0
-    for _ in range(len(arr)):
-        total+=sum(arr[_])
+    global total
     mean=total/count
     for i in range(N):
         for j in range(M):
-            if arr[i][j]!=0:
-                if arr[i][j] > mean:
-                    arr[i][j]-=1
-                else:
-                    arr[i][j]+=1 
+            if arr[i][j]==0:
+                continue
+            if arr[i][j] > mean:
+                arr[i][j]-=1
+                total-=1
+            elif arr[i][j]<mean:
+                arr[i][j]+=1 
+                total+=1
 
-total=0
 for x,d,k in rotation:
-    m=1
-    while(x*m-1<N):
-        rotate((x*m-1), d, k, array)
-        m+=1
-    check=copy.deepcopy(array)
+    m=x
+    while(x-1<N):
+        rotate((x-1), d, k)
+        x+=m
     visited=[[0]*M for _ in range(N)]
     not_mean=False
     for i in range(N):
         for j in range(M):
-            if visited[i][j]==0:
-                visited[i][j]=1
-                visited, array, rem=BFS(i, j, check, visited)
-                if rem==True:
-                    not_mean=True
+            if visited[i][j]!=0:
+                continue
+            visited[i][j]=1
+            if array[i][j]==0:
+                continue
+            queue.appendleft([i, j, array[i][j]])
+            visited, rem=BFS(visited)
+            if not_mean==True:
+                continue
+            if rem==True:
+                not_mean=True
+    if total==0 and count==0:
+        print(total)
+        exit(0)
     if not_mean==False:
         find_mean(array)
-for i in range(len(array)):
-    total+=sum(array[i])
+
 print(total)
