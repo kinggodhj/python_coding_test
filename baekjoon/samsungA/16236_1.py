dbba3357ae80033557ac92d29fa5b5a3447361a5
min_kinggod <heeeee123@ajou.ac.kr>
2021-04-08 14:43:07 +0900

    BFS and sort

--- baekjoon/samsungA/16236_1.py
+++ baekjoon/samsungA/16236_1.py
@@ -12,25 +12,34 @@ for i in range(N):
             shark_x=i
             shark_y=j
 
-def eating(sx, sy, time, ate, size, arr, visit):
+def eating(sx, sy, time, size, arr, visit):
     queue=deque([])
     queue.appendleft([sx, sy, time])
     visit[sx][sy]=1
+    answer=[]
+    cand=None
     while queue:
         x, y, t=queue.pop()
         for i in range(4):
             dx, dy=df[i]
             if 0<=x+dx<N and 0<=y+dy<N:
                 if 0<arr[x+dx][y+dy]<size and visit[x+dx][y+dy]==0:
-                    arr[sx][sy]=0
-                    arr[x+dx][y+dy]=9
-                    ate+=1
-                    return [arr, x+dx, y+dy, ate, t+1]
+                    if cand==None:
+                        cand=t+1
+                        visit[x+dx][y+dy]=1
+                        answer.append([x+dx, y+dy, t+1])
+                    elif t+1==cand:
+                        visit[x+dx][y+dy]=1
+                        answer.append([x+dx, y+dy, t+1])
                 elif arr[x+dx][y+dy]==0 or arr[x+dx][y+dy]==size:
                     if visit[x+dx][y+dy]==0:
-                        visit[x+dx][y+dy]=1
-                        queue.appendleft([x+dx, y+dy, t+1])
-    return None
+                        if cand==None:
+                            visit[x+dx][y+dy]=1
+                            queue.appendleft([x+dx, y+dy, t+1])
+                        elif t+1<cand:
+                            visit[x+dx][y+dy]=1
+                            queue.appendleft([x+dx, y+dy, t+1])
+    return answer
 
 time=0
 size=2
@@ -38,11 +47,15 @@ ate=0
 while(1):
     visit=[[0]*N for _ in range(N)]
     visit[shark_x][shark_y]=1
-    if time==10:
-        breakpoint
-    result=eating(shark_x, shark_y, time, ate, size, array, visit)
-    if type(result)==list:
-        array, shark_x, shark_y, ate, time=result
+    result=eating(shark_x, shark_y, time, size, array, visit)
+    if len(result)>0:
+        result.sort()
+        x, y, time=result[0]
+        array[shark_x][shark_y]=0
+        array[x][y]=9
+        shark_x=x
+        shark_y=y
+        ate+=1
         if ate==size:
             size+=1
             ate=0 
